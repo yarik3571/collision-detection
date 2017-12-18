1df98ae3c89c4ee279294c9fd0a69cb0b7a877d7
yarik <yarplus2012@gmail.com>
2017-12-18 05:39:16 +0200

    added minmaxScala method

--- src/main/scala/pliusnin/geometry/shape/Projection.scala
+++ src/main/scala/pliusnin/geometry/shape/Projection.scala
@@ -12,8 +12,13 @@ object Projection {
     p.project(shape)(axis)
   }
 
+  def minMaxScalar[A <: Shape[_], B <: GVector[_]](shape: A)(axis: B)(implicit p: Projector[A, B]): (Double, Double) = {
+    p.minMaxScalar(shape)(axis)
+  }
+
   trait Projector[A <: Shape[_], B <: GVector[_]] {
     def project(shape: A)(axis: B): B
+    def minMaxScalar(shape: A)(axis: B): (Double, Double)
   }
 
   implicit val polygonProjector = new Projector[Polygon, Vector2d] {
@@ -28,6 +33,18 @@ object Projection {
         .maxBy(GVector.lengthSqr[Point2d, Vector2d])
       projected
     }
+
+    override def minMaxScalar(shape: Polygon)(axis: Vector2d): (Double, Double) = {
+      val vectors = shape.verticies.map(p => Vector2d(p.x, p.y))
+      val firstScalar = GVector.dot[Point2d, Vector2d](vectors.head, axis)
+      vectors.foldLeft((firstScalar, firstScalar)) {
+        case ((min, max), v) =>
+          val vScalar = GVector.dot[Point2d, Vector2d](v, axis)
+          val minUpd = if (vScalar < min) vScalar else min
+          val maxUpd = if (vScalar > max) vScalar else max
+          (minUpd, maxUpd)
+      }
+    }
   }
 
   implicit val circleProjector = new Projector[Circle, Vector2d] {
@@ -38,6 +55,25 @@ object Projection {
         normAxis.x * d,
         normAxis.y * d
       )
+    }
+
+    override def minMaxScalar(shape: Circle)(axis: Vector2d): (Double, Double) = {
+      val r = shape.radius
+      val normAxis = GVector.normalize[Point2d, Vector2d](axis)
+      val start =  Vector2d(
+        normAxis.x * r,
+        normAxis.y * r
+      )
+      val end = Vector2d(
+        normAxis.x * -r,
+        normAxis.y * -r
+      )
+      val startScalar = GVector.dot[Point2d, Vector2d](start, axis)
+      val endScalar = GVector.dot[Point2d, Vector2d](end, axis)
+
+      val minScalar = math.min(startScalar, endScalar)
+      val maxScalar = math.max(startScalar, endScalar)
+      (minScalar, maxScalar)
     }
   }
 
@@ -53,6 +89,18 @@ object Projection {
         .maxBy(GVector.lengthSqr[Point3d, Vector3d])
       projected
     }
+
+    override def minMaxScalar(shape: FaceShape)(axis: Vector3d): (Double, Double) = {
+      val vectors = shape.verticies.map(p => Vector3d(p.x, p.y, p.z))
+      val firstScalar = GVector.dot[Point3d, Vector3d](vectors.head, axis)
+      vectors.foldLeft((firstScalar, firstScalar)) {
+        case ((min, max), v) =>
+          val vScalar = GVector.dot[Point3d, Vector3d](v, axis)
+          val minUpd = if (vScalar < min) vScalar else min
+          val maxUpd = if (vScalar > max) vScalar else max
+          (minUpd, maxUpd)
+      }
+    }
   }
 
   implicit val sphereProjector = new Projector[Sphere, Vector3d] {
@@ -64,6 +112,27 @@ object Projection {
         normAxis.y * d,
         normAxis.z * d
       )
+    }
+
+    override def minMaxScalar(shape: Sphere)(axis: Vector3d): (Double, Double) = {
+      val r = shape.radius
+      val normAxis = GVector.normalize[Point3d, Vector3d](axis)
+      val start =  Vector3d(
+        normAxis.x * r,
+        normAxis.y * r,
+        normAxis.z * r
+      )
+      val end = Vector3d(
+        normAxis.x * -r,
+        normAxis.y * -r,
+        normAxis.z * -r
+      )
+      val startScalar = GVector.dot[Point3d, Vector3d](start, axis)
+      val endScalar = GVector.dot[Point3d, Vector3d](end, axis)
+
+      val minScalar = math.min(startScalar, endScalar)
+      val maxScalar = math.max(startScalar, endScalar)
+      (minScalar, maxScalar)
     }
   }
 
